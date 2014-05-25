88227f34fe669320832bcb83ecba0c780d1b366c
ZPaulo <jose.p.s.olive@gmail.com>
2014-05-25 03:05:55 +0100

    errors missing
    Signed-off-by: ZPaulo <jose.p.s.olive@gmail.com>

--- Queue.c
+++ Queue.c
@@ -1,83 +1,69 @@
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
- #include <semaphore.h>
 
-typedef unsigned long QueueElem; 
-typedef struct 
-{ 
- QueueElem *v; // pointer to the queue buffer unsigned 
- int capacity; // queue capacity 
- unsigned int first; // head of the queue 
- unsigned int last; // tail of the queue 
- sem_t empty; // semaphores and mutex for implementing the 
- sem_t full; // producer-consumer paradigm 
- pthread_mutex_t mutex; 
+typedef unsigned long QueueElem;
+typedef struct {
+	QueueElem *v; // pointer to the queue buffer unsigned
+	int capacity; // queue capacity
+	unsigned int first; // head of the queue
+	unsigned int last; // tail of the queue
+	sem_t empty; // semaphores and mutex for implementing the
+	sem_t full; // producer-consumer paradigm
+	pthread_mutex_t mutex;
 } CircularQueue;
- 
-//------------------------------------------------------------------------------------------ 
-// Allocates space for circular queue 'q' having 'capacity' number of elements 
-// Initializes semaphores & mutex needed to implement the producer-consumer paradigm 
-// Initializes indexes of the head and tail of the queue 
-// TO DO BY STUDENTS: ADD ERROR TESTS TO THE CALLS & RETURN a value INDICATING (UN)SUCESS 
- 
-void queue_init(CircularQueue **q, unsigned int capacity) // TO DO: change return value 
-{ 
- *q = (CircularQueue *) malloc(sizeof(CircularQueue)); 
- sem_init(&((*q)->empty), 0, capacity); 
- sem_init(&((*q)->full), 0, 0); 
- pthread_mutex_init(&((*q)->mutex), NULL); 
- (*q)->v = (QueueElem *) malloc(capacity * sizeof(QueueElem)); 
- (*q)->capacity = capacity; 
- (*q)->first = 0; 
- (*q)->last = 0; 
-} 
- 
-//------------------------------------------------------------------------------------------ 
-// Inserts 'value' at the tail of queue 'q' 
- 
-void queue_put(CircularQueue *q, QueueElem value) 
-{  
-	
-   pthread_mutex_lock(&(q->mutex)); 
-sem_wait(&(q->empty));
-      q->last=(q->last+1)%q->capacity;
-      q->v[q->last]=x;
-      if(q->first == 0) 
-         q->first=q->last;
-   sem_post(&(q->full));
-   pthread_mutex_unlock(&(q->mutex)); 
-}
- 
-QueueElem queue_get(CircularQueue *q) 
-{ 
-	pthread_mutex_lock(&(q->mutex)); 
-   QueueElem data;
 
-  sem_wait(&(q->full));
-      data=q->v[q->first];
-      if(q->first==q->last)
-         q->first=q->last=0;
-      else
-         q->first=(q->first+1)%q->capacity;
- 
-      sem_post(&(q->empty));
-      pthread_mutex_unlock(&(q->mutex)); 
-   return data;
-} 
- 
-//------------------------------------------------------------------------------------------ 
-// Frees space allocated for the queue elements and auxiliary management data 
-// Must be called when the queue is no more needed 
- 
-void queue_destroy(CircularQueue *q) 
-{ 
-   pthread_mutex_lock(&(q->mutex)); 
-   free(q->v);
-   
-   sem_destroy(&(q->empty));
-   sem_destroy(&(q->full));
-      pthread_mutex_unlock(&(q->mutex)); 
-      pthread_mutex_destroy(&(q->mutex));
-} 
-//
+//------------------------------------------------------------------------------------------
+// Allocates space for circular queue 'q' having 'capacity' number of elements
+// Initializes semaphores & mutex needed to implement the producer-consumer paradigm
+// Initializes indexes of the head and tail of the queue
+// TO DO BY STUDENTS: ADD ERROR TESTS TO THE CALLS & RETURN a value INDICATING (UN)SUCESS
+
+int queue_init(CircularQueue **q, unsigned int capacity) // TO DO: change return value
+{
+	*q = (CircularQueue *) malloc(sizeof(CircularQueue));
+	sem_init(&((*q)->empty), 0, capacity);
+	sem_init(&((*q)->full), 0, 0);
+	pthread_mutex_init(&((*q)->mutex), NULL);
+	(*q)->v = (QueueElem *) malloc(capacity * sizeof(QueueElem));
+	(*q)->capacity = capacity;
+	(*q)->first = 0;
+	(*q)->last = 0;
+	return 0;
+}
+
+void queue_put(CircularQueue *q, QueueElem value) {
+
+	sem_wait(&(q->empty));
+	pthread_mutex_lock(&(q->mutex));
+	q->v[q->last++] = value;
+	if (q->last == q->capacity)
+		q->last = 0;
+	pthread_mutex_unlock(&(q->mutex));
+	sem_post(&(q->full));
+}
+
+QueueElem queue_get(CircularQueue *q) {
+	QueueElem data;
+
+	sem_wait(&(q->full));
+	pthread_mutex_lock(&(q->mutex));
+	data = q->v[q->first++];
+	if (q->first == q->capacity)
+		q->first = 0;
+
+	pthread_mutex_unlock(&(q->mutex));
+	sem_post(&(q->empty));
+	return data;
+}
+
+void queue_destroy(CircularQueue *q) {
+	pthread_mutex_lock(&(q->mutex));
+	free(q->v);
+
+	sem_destroy(&(q->empty));
+	sem_destroy(&(q->full));
+	pthread_mutex_unlock(&(q->mutex));
+	pthread_mutex_destroy(&(q->mutex));
+}
--- Test.c
+++ Test.c
@@ -1,129 +1,132 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <errno.h>
 #include "Queue.c"
-
+#define QUEUE_SIZE 10
 
 void *filter(void *arg);
 void *start(void *arg);
 
-int* primes;
+QueueElem* primes;
 int pos;
 int N;
 pthread_mutex_t mutex;
+sem_t final;
+int end;
 
-int compare(const void * a, const void * b) 
-{
- return (*(int*) a - *(int*) b);
+int compare(const void * a, const void * b) {
+	return (*(int*) a - *(int*) b);
 }
 
-void insertPrime(QueueElem newPrime)
-{
-	pthread_mutex_lock(&mutex); 
+void insertPrime(QueueElem newPrime) {
+	pthread_mutex_lock(&mutex);
 	primes[pos++] = newPrime;
-	pthread_mutex_unlock(&mutex); 
+	pthread_mutex_unlock(&mutex);
 }
 
-int main(int argc, char *argv[]) 
-{
- N = strtol(argv[1], NULL, 0);
- int nprimes = N * log(n);
- pthread_mutex_init(&mutex, NULL); 
+int main(int argc, char *argv[]) {
 
- primes = (int*) malloc(sizeof(int) * nprimes);
- pos = 0;
+	int i;
 
- pthread_t f;
- char *msg1 = "First Filter";
-
- pthread_create(&f, NULL, start, (void *) msg1);
-
- qsort(primes, nprimes, sizeof(int), compare);
-
- while (primes != '\0') 
- {
-  printf("%d\n", *primes++);
- }
- free(primes);
-
- return 0;
-
-}
-
-void *start(void *arg) 
-{
-
- insertPrime(2);
- if (N > 2) 
- {
-  pthread_t f;
-
-  CircularQueue *q;
-  queue_init(&q, QUEUE_SIZE);
-
-  pthread_create(&f, NULL, filter, q);
-
-  int i = 3;
-  while (i < N) {
-   queue_put(q, i);
-   i += 2;
-  }
-  queue_put(q, 0);
-
- 
- }
- else
- {
-
- }
-
- return NULL;
-}
-
-void *filter(void *arg) 
-{
-	QueueElem newPrime = queue_get(arg);
-	if(newPrime > (int) (sqrt(N)))
-	{
-		insertPrime(newPrime);
-		 newPrime = queue_get(arg);
-		 if(newPrime != 0)
-		 {
-		 	do
-		 	{
-		 		insertPrime(newPrime);
-		 		newPrime = queue_get(arg);
-
-		 	}while(newPrime != 0);
-		 }
-		 	//semaforo
-		 
+	if (argc != 2) {
+		printf("Usage: ./primes n\n");
+		exit(E2BIG);
 	}
-	else
+	for (i = 0; argv[1][i] != '\0'; ++i) {
+		if(argv[1][i]<'0' || argv[1][i]>'9')
+		{
+			printf("Number must be a value!\n");
+			exit(EINVAL);
+		}
+	}
+	N = strtol(argv[1], NULL, 0);
+	if(N<2)
 	{
-		CircularQueue *q; 
+		printf("Number must be atleast 2!(no primes lower than that)\n");
+		exit(ERANGE);
+	}
+	int nprimes = ceil(1.2 * (N / log(N))) + 1;
+	sem_init(&final, 1, end);
+	pthread_mutex_init(&mutex, NULL);
+
+	primes = (QueueElem*) malloc(sizeof(QueueElem) * nprimes);
+	end = pos = 0;
+
+	pthread_t f;
+	char *msg1 = "First Filter";
+
+	pthread_create(&f, NULL, start, (void *) msg1);
+
+	sem_wait(&final);
+	sem_destroy(&final);
+	pthread_mutex_unlock(&mutex);
+	pthread_mutex_destroy(&mutex);
+
+	qsort(primes, pos, sizeof(QueueElem), compare);
+
+	printf("Primes:\n");
+	for (i = 0; i < pos; ++i)
+		printf("%lu\n", primes[i]);
+
+	printf("That makes a total of %d prime(s)\n", i);
+	free(primes);
+	return 0;
+
+}
+
+void *start(void *arg) {
+
+	insertPrime(2);
+	if (N > 2) {
+		pthread_t f;
+
+		CircularQueue *q;
 		queue_init(&q, QUEUE_SIZE);
-		
+
+		pthread_create(&f, NULL, filter, q);
+
+		QueueElem i = 3;
+		while (i <= N) {
+			queue_put(q, i);
+			i += 2;
+		}
+		queue_put(q, 0);
+	} else {
+		sem_post(&final);
+	}
+
+	return NULL;
+}
+
+void *filter(void *arg) {
+	QueueElem newPrime = queue_get(arg);
+	if (newPrime > (QueueElem) (sqrt(N))) {
+		insertPrime(newPrime);
+		while ((newPrime = queue_get(arg)) != 0) {
+			insertPrime(newPrime);
+		}
+
+		queue_destroy(arg);
+		sem_post(&final);
+
+	} else {
+		insertPrime(newPrime);
+		CircularQueue *q;
+		queue_init(&q, QUEUE_SIZE);
 
 		pthread_t f;
-  		pthread_create(&f, NULL, filter, q);
-
-		QueueElem nonMult = queue_get(arg);
-  		if(nonMult != 0)
-  		{
-  			do
-  			{
-  				if( nonMult % newPrime != 0)
-  					queue_put(q, nonMult);
-
-  				nonMult = queue_get(arg);
-  			}while(nonMult != 0);
-
-  		}
-  		queue_destroy(arg);
-  		queue_put(q, 0);
-  		
-  		insertPrime(newPrime);
+		pthread_create(&f, NULL, filter, q);
+		QueueElem nonMult;
+		while ((nonMult = queue_get(arg)) != 0) {
+			if ((nonMult % newPrime) != 0) {
+				queue_put(q, nonMult);
+			}
+		}
+		queue_put(q, 0);
+		queue_destroy(arg);
 	}
- 	return NULL;
+	return NULL;
 }
